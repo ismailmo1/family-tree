cabb512b22f111b3ac01772da2082e71735b47dd
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-06 23:31:55 +0100

    wraper func for transactions

--- backend/db/family_db.py
+++ backend/db/family_db.py
@@ -1,10 +1,8 @@
-import logging
-import os
-
 import neo4j
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
-from neo4j.exceptions import ServiceUnavailable
+
+from db.transactions import TransactionType
 
 load_dotenv("./.env")
 
@@ -24,142 +22,48 @@ class FamilyGraph:
     def __exit__(self, type, value, traceback):
         self.driver.close()
 
-    def create_single(self, person_name: str) -> None:
+    def clean_db(self):
         with self.driver.session() as session:
-            # write_transaction takes transaction fn as first arg, and rest are passed as *args, **kwargs to the fn
-            result = session.write_transaction(
-                self._create_and_return_single, person_name
-            )
-        print(f"Created a loner: {person_name}")
+            session.write_transaction(self._clean_db)
+
+    def transaction(self, trans_type: TransactionType):
+        def trans_wrapper(query_func):
+            def wrapper(*args, **kwargs):
+                query, query_args = query_func(*args, **kwargs)
+
+                # create transaction function
+                def trans_fn(tx, query_args):
+                    result = tx.run(query, query_args)
+                    result_list = []
+
+                    for row in result:
+                        # create dictionary mapping of result keys to result values
+                        result_values = row.values(*result.keys())
+                        result_dict = dict(zip(result.keys(), result_values))
+                        result_list.append(result_dict)
+
+                    return result_list
+
+                # use self context manager to close driver after use
+                with self:
+                    with self.driver.session() as session:
+                        if trans_type == TransactionType.READ:
+                            result = session.read_transaction(
+                                trans_fn, query_args
+                            )
+                        if trans_type == TransactionType.WRITE:
+                            result = session.write_transaction(
+                                trans_fn, query_args
+                            )
+
+                    return result
+
+            return wrapper
+
+        return trans_wrapper
 
     @staticmethod
-    def _create_and_return_single(
-        tx: neo4j.Transaction, person_name: str
-    ) -> list[dict[str, str]]:
-        query = (
-            "CREATE (person:Person { name: $person_name }) " "RETURN person"
-        )
-        result = tx.run(query, person_name=person_name)
-        try:
-            return [
-                {
-                    "person": row["person"]["name"],
-                }
-                for row in result
-            ]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error(
-                "{query} raised an error: \n {exception}".format(
-                    query=query, exception=exception
-                )
-            )
-            raise
-
-    def create_marriage(self, person1_name: str, person2_name: str) -> None:
-        with self.driver.session() as session:
-            # write_transaction takes transaction fn as first arg, and rest are passed as *args, **kwargs to the fn
-            result = session.write_transaction(
-                self._create_and_return_marriage, person1_name, person2_name
-            )
-        print(f"Created marriage between: {person1_name} and {person2_name}")
-
-    @staticmethod
-    def _create_and_return_marriage(
-        tx: neo4j.Transaction, person1_name: str, person2_name: str
-    ) -> list[dict[str, str]]:
-        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-        query = (
-            "MATCH (person1:Person { name: $person1_name }) "
-            "MATCH (person2:Person { name: $person2_name }) "
-            "CREATE (person1)-[:SPOUSE_OF]->(person2) "
-            "RETURN person1, person2"
-        )
-        result = tx.run(
-            query, person1_name=person1_name, person2_name=person2_name
-        )
-        try:
-            return [
-                {
-                    "person1": row["person1"]["name"],
-                    "person2": row["person2"]["name"],
-                }
-                for row in result
-            ]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error(
-                "{query} raised an error: \n {exception}".format(
-                    query=query, exception=exception
-                )
-            )
-            raise
-
-    def create_child(
-        self, child_name: str, parent1_name: str, parent2_name: str
-    ) -> None:
-        """Creates a child node and a relationship to parents, order of parents is irrelevant
-
-        Args:
-            child_name (str): child name
-            parent1_name (str): name of first parent
-            parent2_name (str): name of second parent
-        """
-        with self.driver.session() as session:
-            # write_transaction takes transaction fn as first arg, and rest are passed as *args, **kwargs to the fn
-            result = session.write_transaction(
-                self._create_and_return_child,
-                child_name,
-                parent1_name,
-                parent2_name,
-            )
-            for row in result:
-                print(
-                    "Created child: {child} for {parent1} and {parent2}".format(
-                        child=row["child"],
-                        parent1=row["parent1"],
-                        parent2=row["parent2"],
-                    )
-                )
-
-    @staticmethod
-    def _create_and_return_child(
+    def _clean_db(
         tx: neo4j.Transaction,
-        child_name: str,
-        parent1_name: str,
-        parent2_name: str,
-    ) -> list[dict[str, str]]:
-        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-        query = (
-            "MATCH (parent1:Person { name: $parent1_name})"
-            "MATCH (parent2:Person { name: $parent2_name})"
-            "CREATE (child:Person { name: $child_name }) "
-            "CREATE (child)-[:CHILD_OF]-> (parent1)"
-            "CREATE (child)-[:CHILD_OF]->(parent2)"
-            "RETURN child, parent1, parent2"
-        )
-        result = tx.run(
-            query,
-            child_name=child_name,
-            parent1_name=parent1_name,
-            parent2_name=parent2_name,
-        )
-        try:
-            return [
-                {
-                    "parent1": row["parent1"]["name"],
-                    "parent2": row["parent2"]["name"],
-                    "child": row["child"]["name"],
-                }
-                for row in result
-            ]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error(
-                "{query} raised an error: \n {exception}".format(
-                    query=query, exception=exception
-                )
-            )
-            raise
+    ):
+        tx.run("MATCH (n) DETACH DELETE(n)")
