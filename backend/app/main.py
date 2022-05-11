b278307b0d67dd74cbb052991884e4d49f6f5fd7
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-11 23:53:55 +0100

    added api routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
-from .routers import parents
+from .routers import children, family, parents, people
 
 app = FastAPI()
 
 app.include_router(parents.router)
+app.include_router(family.router)
+app.include_router(children.router)
+app.include_router(people.router)
