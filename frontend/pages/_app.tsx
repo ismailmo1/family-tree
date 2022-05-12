32269c685567c91d83a986b2e3ec1e8a6e0f760c
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-12 22:47:41 +0100

    added chakra-ui

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Layout from '../components/layouts/main'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout >
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
