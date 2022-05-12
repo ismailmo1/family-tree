b77e604939cfcc3f547f60f590bbdd6bfc942071
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-12 22:46:15 +0100

    added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import children, family, parents, people
 
@@ -8,3 +9,15 @@ app.include_router(parents.router)
 app.include_router(family.router)
 app.include_router(children.router)
 app.include_router(people.router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
