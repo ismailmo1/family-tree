3640573956d80a88fc1bbb39837bf786e1954ea7
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-15 19:24:48 +0100

    made response type same for person get

--- backend/app/routers/people.py
+++ backend/app/routers/people.py
@@ -14,13 +14,17 @@ def get_person(id: str | None = None, name: str | None = None):
         raise HTTPException(400, "You must provide either an id or name!")
     elif name:
         id_list = find_person_by_name(name)
+        if len(id_list) == 1:
+            id = id_list[0]["id"]
+            return [find_person_properties(id)[0]["props"]]
+
         people_props = []
         for person in id_list:
             people_props.append(
                 find_person_properties(person["id"])[0]["props"]
             )
     else:
-        people_props = find_person_properties(id)
+        people_props = [find_person_properties(id)[0]["props"]]
     return people_props
 
 
