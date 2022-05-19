6ce9cac4137d380c42d1a3d055a9747949f2adc1
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-19 23:20:02 +0100

    flattened page structure

--- frontend/pages/siblings/[person_id].tsx
+++ frontend/pages/siblings/[person_id].tsx
@@ -2,11 +2,11 @@ import { useDisclosure } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import SearchResults from "../../../components/cards/SearchResults";
-import FindPersonModal from "../../../components/modals/FindPersonModal";
-import Header from "../../../components/utils/PageHeader";
-import { PersonMatchResult } from "../../../types/person";
+import { MouseEvent, useEffect, useState } from "react";
+import SearchResults from "../../components/cards/SearchResults";
+import FindPersonModal from "../../components/modals/FindPersonModal";
+import Header from "../../components/utils/PageHeader";
+import { PersonMatchResult } from "../../types/person";
 
 const SiblingsPage: NextPage = () => {
   const router = useRouter();
@@ -29,11 +29,20 @@ const SiblingsPage: NextPage = () => {
     fetchPersonDetails();
   }, [personId]);
 
+  const onCardClick = (e: MouseEvent<HTMLElement>) => {
+    console.log(e.currentTarget.id);
+    onClose();
+  };
+
   return (
     <>
       <Header title="Siblings" clickHandler={onOpen} />
       {siblings ? <SearchResults personMatches={siblings} /> : ""}
-      <FindPersonModal onClose={onClose} isOpen={isOpen} />
+      <FindPersonModal
+        onClose={onClose}
+        isOpen={isOpen}
+        onCardClick={onCardClick}
+      />
       <Link href={`/person/${personId}`}>Back to person details</Link>
     </>
   );
