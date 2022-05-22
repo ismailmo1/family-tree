11109dee9cda515b6a602467ceca582cac702b74
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-22 20:33:39 +0100

    add parents modal

--- frontend/pages/parents/[person_id].tsx
+++ frontend/pages/parents/[person_id].tsx
@@ -1,15 +1,20 @@
-import { Heading } from "@chakra-ui/react";
+import { useDisclosure, useToast } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 import SearchResults from "../../components/cards/SearchResults";
+import FindPersonModal from "../../components/modals/FindPersonModal";
+import Header from "../../components/utils/PageHeader";
 import { PersonMatchResult } from "../../types/person";
+import { addParentSuccessResponse } from "../../types/relationships";
 
 const ParentsPage: NextPage = () => {
   const router = useRouter();
   const { person_id: personId } = router.query;
   const [parentDetails, setParentDetails] = useState<PersonMatchResult[]>();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
 
   useEffect(() => {
     if (!personId) {
@@ -21,16 +26,40 @@ const ParentsPage: NextPage = () => {
       setParentDetails(data);
     };
     fetchParentDetails();
-  }, [personId]);
+  }, [personId, isOpen]);
+
+  const onCardClick = async (e: MouseEvent<HTMLElement>) => {
+    const parentId = e.currentTarget.id;
+    const res = await fetch(
+      `http://localhost:8000/parents/?child_id=${personId}&parent_id=${parentId}`,
+      { method: "POST" }
+    );
+    const addParentData: addParentSuccessResponse = await res.json();
+
+    toast({
+      title: "Parent Added",
+      description: `${addParentData.parent.name} added as parent!`,
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+    });
+    onClose();
+  };
 
   return (
     <>
-      <Heading py={5}>Parents</Heading>
+      <Header title="Parents" clickHandler={onOpen} />
       {parentDetails ? (
         <SearchResults personMatches={parentDetails} />
       ) : (
         "searching for parents..."
       )}
+      <FindPersonModal
+        relation="parent"
+        onClose={onClose}
+        isOpen={isOpen}
+        onCardClick={onCardClick}
+      />
       <Link href={`/person/${personId}`}>Back to person details</Link>
     </>
   );
