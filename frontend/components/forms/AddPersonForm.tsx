eed9aacb19721b3d0048c0047dbc7ab03f2da58f
Ismail Mohammed <ismailmo4@gmail.com>
2022-06-10 22:34:30 +0000

    fixed dependencies

--- frontend/components/forms/AddPersonForm.tsx
+++ frontend/components/forms/AddPersonForm.tsx
@@ -22,22 +22,25 @@ const CreatePersonForm: React.FC<{ onAddPerson?: (id: string) => void }> = ({
   const toast = useToast();
   const router = useRouter();
 
-  const renderToastContent = (id: string, name: string) => (
-    <Alert
-      status="success"
-      as="button"
-      onClick={() => router.push(`/person/${id}`)}
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      textAlign="center"
-    >
-      <AlertIcon />
-      <AlertTitle>{name} added successfully!</AlertTitle>
-      <AlertDescription>
-        Click me to go to their page to add relationships
-      </AlertDescription>
-    </Alert>
+  const renderToastContent = useCallback(
+    (id: string, name: string) => (
+      <Alert
+        status="success"
+        as="button"
+        onClick={() => router.push(`/person/${id}`)}
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+      >
+        <AlertIcon />
+        <AlertTitle>{name} added successfully!</AlertTitle>
+        <AlertDescription>
+          Click me to go to their page to add relationships
+        </AlertDescription>
+      </Alert>
+    ),
+    [router]
   );
 
   const addPerson = useCallback(async () => {
@@ -57,12 +60,12 @@ const CreatePersonForm: React.FC<{ onAddPerson?: (id: string) => void }> = ({
     });
     setIsAdding(false);
     onAddPerson && onAddPerson(data.id);
-  }, []);
+  }, [onAddPerson, renderToastContent, toast]);
 
   return (
     <>
       <FormControl>
-        <FormLabel htmlFor="name">Enter person's name</FormLabel>
+        <FormLabel htmlFor="name">{"Enter person's name"}</FormLabel>
         <Input id="name" ref={personName} />
         <Center py="5">
           <Button onClick={addPerson}>
