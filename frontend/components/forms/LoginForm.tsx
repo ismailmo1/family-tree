14bf83e2a26a5ec02e48e2a09694361c528a54b7
Ismail Mohammed <ismailmo4@gmail.com>
2022-06-13 22:47:16 +0000

    frontend login form

--- frontend/components/forms/LoginForm.tsx
+++ frontend/components/forms/LoginForm.tsx
@@ -4,34 +4,71 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Spinner,
+  ToastId,
+  useToast,
+  UseToastOptions,
 } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { useFetch } from "use-http";
+import { API_URL } from "../../globals";
 
 interface JWTResponse {
   access_token: string;
   token_type: string;
 }
 const LoginForm = () => {
-  const { loading, error, response, request } = useFetch<JWTResponse>(
-    "http://localhost:8000"
-  );
+  const toast = useToast();
+  const errorToastIdRef = useRef<ToastId>();
+  const [usernameInput, setUsernameInput] = useState<string>("");
+  const [passwordInput, setPasswordInput] = useState<string>("");
+  const { loading, error, response, request } = useFetch<JWTResponse>(API_URL);
   const loginUser = async () => {
-    const res = await request.post("/auth/token", {
-      username: "user1",
-      password: "secret",
-    });
+    const data = new FormData();
+    data.append("username", usernameInput);
+    data.append("password", passwordInput);
+    const res = await request.post("/auth/token", data);
     console.log(res.access_token);
-
-    return res.access_token;
+    const successToastOptions: UseToastOptions = {
+      title: "logged in!",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+    };
+    toast(successToastOptions);
+    // return res.access_token;
   };
+  if (response.ok === false) {
+    const errorToastOptions: UseToastOptions = {
+      title: "Login Failed!",
+      description: `${response.status}: ${response.statusText}`,
+      status: "error",
+      duration: 5000,
+      isClosable: true,
+    };
+    if (errorToastIdRef.current) {
+      toast.update(errorToastIdRef.current, errorToastOptions);
+    } else {
+      errorToastIdRef.current = toast(errorToastOptions);
+    }
+  }
+
+  if (loading) {
+    return <Spinner size={"xl"} />;
+  }
+
   return (
     <FormControl>
       <FormLabel htmlFor="username">Username</FormLabel>
-      <Input id="username" />
-      <FormLabel htmlFor="passord">Password</FormLabel>
-      <Input id="password" type="password" />
+      <Input id="username" onChange={(e) => setUsernameInput(e.target.value)} />
+      <FormLabel htmlFor="password">Password</FormLabel>
+      <Input
+        id="password"
+        type="password"
+        onChange={(e) => setPasswordInput(e.target.value)}
+      />
       <Center py="5">
-        <Button onSubmit={loginUser}>Login</Button>
+        <Button onClick={loginUser}>Login</Button>
       </Center>
     </FormControl>
   );
