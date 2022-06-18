1ac670b702097e445816636c2ba33adbeab0dda6
Ismail Mohammed <ismailmo4@gmail.com>
2022-06-18 21:53:09 +0000

    Add family tree svg

--- frontend/components/cards/FamilyTree.tsx
+++ frontend/components/cards/FamilyTree.tsx
@@ -8,9 +8,45 @@ import {
   Wrap,
   WrapItem,
 } from "@chakra-ui/react";
+import * as d3 from "d3";
+import dTree from "d3-dtree";
+import _ from "lodash";
+import { useRef } from "react";
 import { NuclearFamily } from "../../types/family";
 import SimplePersonCard from "./SimplePersonCard";
+
 const FamilyTree: React.FC<{ family: NuclearFamily }> = ({ family }) => {
+  window.d3 = d3;
+  const graphDiv = useRef<HTMLDivElement>(null);
+  if (graphDiv?.current?.children.length) {
+    // remove graph on subsequent renders
+    console.log("removing graph");
+
+    graphDiv.current.removeChild(graphDiv.current.children[0]);
+    console.log("creating graph");
+    console.log(family);
+
+    dTree.init([
+      {
+        name: family.parents[0].name, // The name of the node
+        class: "node", // The CSS class of the node
+        textClass: "nodeText", // The CSS class of the text in the node
+        depthOffset: 1, // Generational height offset
+        marriages: [
+          {
+            // Marriages is a list of nodes
+            spouse: {
+              // Each marriage has one spouse
+              name: family.parents[1].name,
+            },
+            children: family.children,
+          },
+        ],
+        extra: {}, // Custom data passed to renderers
+      },
+    ]);
+  }
+
   const parentCards = (
     <>
       <Heading>Parents</Heading>
@@ -51,6 +87,7 @@ const FamilyTree: React.FC<{ family: NuclearFamily }> = ({ family }) => {
 
   return (
     <Container minWidth={"60%"} py={5}>
+      <div ref={graphDiv} id="graph"></div>
       <VStack>
         {parentCards}
         {siblingCards}
