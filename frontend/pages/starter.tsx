f2dff3ce175d4b409a249a4cfb9060aa89509ff8
Ismail Mohammed <ismailmo4@gmail.com>
2022-06-26 23:41:19 +0000

    Add user greeting

--- frontend/pages/starter.tsx
+++ frontend/pages/starter.tsx
@@ -7,12 +7,14 @@ import {
 import { Center, Heading, SimpleGrid } from "@chakra-ui/react";
 import { NextPage } from "next";
 import StarterCard from "../components/cards/StarterCard";
+import { useAuth } from "../hooks/use-auth";
 
 const Starter: NextPage = () => {
+  const { user } = useAuth();
   return (
     <>
       <Center py="10">
-        <Heading>Welcome To Family Tree!</Heading>
+        <Heading>Welcome To Family Tree {user?.name}!</Heading>
       </Center>
       <SimpleGrid spacing="20px" columns={{ sm: 1, md: 2 }}>
         <StarterCard
