dc8e059fce2744d8f6c6dfe180ecba235597ad82
Ismail Mohammed <ismailmo4@gmail.com>
2022-09-19 21:27:18 +0100

    add active card

--- frontend/components/tree/FamilyTree.tsx
+++ frontend/components/tree/FamilyTree.tsx
@@ -11,52 +11,63 @@ import {
 import * as d3 from "d3";
 import _ from "lodash";
 import dTree from "d3-dtree";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { NuclearFamily } from "../../types/family";
 import SimplePersonCard from "../cards/SimplePersonCard";
 import { useRouter } from "next/router";
 import styles from "./Tree.module.css";
 import { PersonMatchResult } from "../../types/person";
+import PersonCard from "../cards/PersonCard";
 
 const FamilyTree: React.FC<{ family: NuclearFamily }> = ({ family }) => {
-  const [activeNode, setActiveNode] = useState<PersonMatchResult>(
-    family.children[0]
-  );
   const router = useRouter();
+  const { person_id: personId } = router.query;
+  const currPerson = [...family.parents, ...family.children].find(
+    (person) => person.id == personId
+  );
+  const [activeNode, setActiveNode] = useState<PersonMatchResult | undefined>(
+    currPerson
+  );
+
+  // HACK: get d3 and lodash to work this way
   window.d3 = d3;
-  const treeData = [
-    {
-      name: family.parents[0]?.name || "No Parent Found", // The name of the node
-      class: styles.mainNode, // The CSS class of the node
-      textClass: "nodeText", // The CSS class of the text in the node
-      depthOffset: 1, // Generational height offset
-      marriages: [
-        {
-          // Marriages is a list of nodes
-          spouse: {
-            // Each marriage has one spouse
-            name: family.parents[1]?.name || "No Parent Found",
-            class: styles.spouseNode,
-            extra: { id: family.parents[1]?.id },
+  window._ = _;
+
+  const treeData = useMemo(
+    () => [
+      {
+        name: family.parents[0]?.name || "No Parent Found", // The name of the node
+        class: styles.mainNode, // The CSS class of the node
+        textClass: "nodeText", // The CSS class of the text in the node
+        depthOffset: 1, // Generational height offset
+        marriages: [
+          {
+            // Marriages is a list of nodes
+            spouse: {
+              // Each marriage has one spouse
+              name: family.parents[1]?.name || "No Spouse Found",
+              class: styles.spouseNode,
+              extra: { id: family.parents[1]?.id },
+            },
+            children: family.children?.map((child) => {
+              return {
+                ...child,
+                class: styles.childNode,
+                extra: { id: child.id },
+              }; // add id to extra obj to allow callbacks
+            }),
           },
-          children: family.children?.map((child) => {
-            return {
-              ...child,
-              class: styles.childNode,
-              extra: { id: child.id },
-            }; // add id to extra obj to allow callbacks
-          }),
-        },
-      ],
-      extra: { id: family.parents[0]?.id }, // Custom data passed to renderers
-    },
-  ];
+        ],
+        extra: { id: family.parents[0]?.id }, // Custom data passed to renderers
+      },
+    ],
+    [family]
+  );
   const graphDiv = useRef<HTMLDivElement>(null);
   useEffect(() => {
     // HACK remove tree if it exists already
     if (graphDiv?.current?.children.length) {
       // remove graph on subsequent renders
-      console.log("removing graph");
 
       graphDiv.current.removeChild(graphDiv.current.children[0]);
     }
@@ -89,18 +100,21 @@ const FamilyTree: React.FC<{ family: NuclearFamily }> = ({ family }) => {
           node += 'id="node' + id + '">\n';
           node += textRenderer(name, extra, textClass);
           node += "</div>";
-          console.log(x, y, name);
 
           return node;
         },
       },
     });
-  }, [_, treeData]); //HACK add dependency to lodash otherwise it doesn't import properly
+  }, [treeData]);
 
   const activeNodeCard = (
     <>
       <Center>
-        <SimplePersonCard name={activeNode.name} id={activeNode.id} />
+        {activeNode ? (
+          <PersonCard name={activeNode.name} id={activeNode.id} />
+        ) : (
+          ""
+        )}
       </Center>
     </>
   );
@@ -109,6 +123,9 @@ const FamilyTree: React.FC<{ family: NuclearFamily }> = ({ family }) => {
     <Container maxWidth="1700px" py={5}>
       <div className={styles.graph} ref={graphDiv} id="graph"></div>
       <VStack>
+        <Heading size={"md"} marginTop={"10px"}>
+          Pick a family member in the tree above:
+        </Heading>
         <div>{activeNodeCard}</div>
       </VStack>
     </Container>
