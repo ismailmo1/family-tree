d6d8490584e0232dc4e750feb0452afcc54f44c3
Ismail Mohammed <ismailmo4@gmail.com>
2022-10-14 22:16:43 +0000

    Add parents to siblings and overwrite dummy

--- backend/app/db/transactions/delete.py
+++ backend/app/db/transactions/delete.py
@@ -1,3 +1,5 @@
+"""DB delete transactions to remove people and relationships"""
+
 from app.db import family_graph
 
 
@@ -12,7 +14,7 @@ def delete_person(person_id: str):
     return family_graph.write_query(cypher_query, {"person_id": person_id})
 
 
-def delete_relationships(person_id: str):
+def delete_all_relationships(person_id: str):
     """remove all relationships of a person
 
     Args:
@@ -21,3 +23,19 @@ def delete_relationships(person_id: str):
     cypher_query = "MATCH (p:Person {id:$person_id})-[rel]-() DELETE rel"
 
     return family_graph.write_query(cypher_query, {"person_id": person_id})
+
+
+def delete_relationship(person_id_1: str, person_id_2: str):
+    """Delete relationship between two people. This will delete any and all
+    direct relationships between `person_id_1` and `person_id_2`.
+
+    Args:
+        person_id_1 (str): id of person 1
+        person_id_2 (str): id of person 1
+    """
+    cypher_query = """MATCH (p:Person {id:$person_id_1})-[rel]-p:Person {id:$person_id_2})
+                        DELETE rel"""
+
+    return family_graph.write_query(
+        cypher_query, {"person_id_1": person_id_1, "person_id_2": person_id_2}
+    )
--- backend/app/routers/parents.py
+++ backend/app/routers/parents.py
@@ -1,7 +1,11 @@
-from app.db.transactions.find import find_parents
-from app.db.transactions.update import add_parent
+""""Routes to add and get parent data"""
+
+from app.db.transactions.find import find_parents, find_siblings
 from app.routers import auth
+from app.db.transactions.update import add_parent
+from app.db.transactions.delete import delete_relationship
 from fastapi import APIRouter, Depends
+
 
 router = APIRouter(
     prefix="/parents", dependencies=[Depends(auth.get_current_user)]
@@ -10,9 +14,31 @@ router = APIRouter(
 
 @router.get("/")
 def get_parents(id: str):
+    """return all parents of person"""
     return find_parents(id)
 
 
 @router.post("/")
 def post_parents(child_id: str, parent_id):
+    """create parent-child relationship between `parent_id` and `child_id`.
+
+    Also adds parent to all of `child_id`'s siblings and parent_id will replace a
+    dummy parent if they exist on `child_id`"""
+    # HACK when we add step parent functionality - this will need some more thought
+    # maybe just an extra argument for map of siblings to parent name
+    # so we can see which parents are shared between which siblings
+
+    # replace dummy parents if they exist
+    current_parents = find_parents(child_id)
+    if current_parents:
+        for parent in current_parents:
+            if parent["name"] == "dummy_parent":
+                delete_relationship(parent["id"], child_id)
+                # only remove one dummy parent (since we're only adding one real parent)
+                break
+    # add parents for all siblings of this child if they exist
+    siblings = find_siblings(child_id)
+    if siblings:
+        for sibling in siblings:
+            add_parent(sibling["id"], parent_id)
     return add_parent(child_id, parent_id)[0]
