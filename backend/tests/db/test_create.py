ab2b64aebd26d994013ea7726bcc5c2279ee9e6b
Ismail Mohammed <ismailmo4@gmail.com>
2022-11-12 00:46:15 +0000

    update db create tests

--- backend/tests/db/test_create.py
+++ backend/tests/db/test_create.py
@@ -1,43 +1,58 @@
-from app.db.transactions import create, delete, find
+from unittest import mock
+
+from app.db import FamilyGraph
+from app.db.transactions import create
 
 
-def test_create_person():
-    created_person_id = create.create_person("create_person_test")[0]["id"]
-    found_person = find.find_person_by_id(created_person_id)[0]["props"]
+class TestCreate:
+    def test_create_person(self, test_graph: FamilyGraph):
+        with mock.patch("app.db.transactions.create.family_graph", test_graph):
+            created_person_id = create.create_person("create_person_test")[0][
+                "id"
+            ]
+            found_person = test_graph.write_query("MATCH (p) RETURN p")[0]["p"]
 
-    assert created_person_id == found_person["id"]
+        assert created_person_id == found_person["id"]
 
-    # cleanup created person
-    delete.delete_person(created_person_id)
+    def test_create_child(self, test_graph: FamilyGraph):
+        # create parents
+        parent1_id = "p1"
+        parent2_id = "p2"
+        test_graph.write_query(
+            "CREATE (:Person { id: $p1_id  }) CREATE (:Person { id: $p2_id  })",
+            {"p1_id": parent1_id, "p2_id": parent2_id},
+        )
 
+        created_family = create.create_child(
+            "test_child", parent1_id, parent2_id
+        )
 
-def test_create_child(temp_person_factory):
-    # create parents
-    family_ids = temp_person_factory(2)
-    parent1_id, parent2_id = family_ids
+        assert created_family[0]["parent1"]["id"] == parent1_id  # type:ignore
+        assert created_family[0]["parent2"]["id"] == parent2_id  # type:ignore
+        assert (
+            created_family[0]["child"]["name"] == "test_child"  # type:ignore
+        )
 
-    created_family = create.create_child("test_child", parent1_id, parent2_id)
+    def test_add_child(self, test_graph: FamilyGraph):
+        # create parents and child
+        parent1_id = "p1"
+        child_id = "c1"
+        test_graph.write_query(
+            "CREATE (:Person { id: $p1_id  }) CREATE (:Person { id: $c1_id  })",
+            {"p1_id": parent1_id, "c1_id": child_id},
+        )
+        created_family = create.add_child(child_id, parent1_id)
 
-    assert created_family[0]["parent1"]["id"] == parent1_id
-    assert created_family[0]["parent2"]["id"] == parent2_id
-    assert created_family[0]["child"]["name"] == "test_child"
+        assert created_family[0]["child"]["id"] == child_id  # type:ignore
 
-    # cleanup child creation
-    delete.delete_person(created_family[0]["child"]["id"])
+    def test_create_marriage(self, test_graph: FamilyGraph):
+        person1_id = "p1"
+        person2_id = "p2"
+        test_graph.write_query(
+            "CREATE (:Person { id: $p1_id  }) CREATE (:Person { id: $p2_id  })",
+            {"p1_id": person1_id, "p2_id": person2_id},
+        )
+        created_marriage = create.create_marriage(person1_id, person2_id)[0]
 
-
-def test_add_child(temp_person_factory):
-    # create parents and child
-    parent1_id, parent2_id, child_id = temp_person_factory(3)
-
-    created_family = create.add_child(child_id, parent1_id, parent2_id)
-
-    assert created_family[0]["child"]["id"] == child_id
-
-
-def test_create_marriage(temp_person_factory):
-    person1_id, person2_id = temp_person_factory(2)
-    created_marriage = create.create_marriage(person1_id, person2_id)[0]
-
-    assert created_marriage["person1"]["id"] == person1_id
-    assert created_marriage["person2"]["id"] == person2_id
+        assert created_marriage["person1"]["id"] == person1_id  # type:ignore
+        assert created_marriage["person2"]["id"] == person2_id  # type:ignore
